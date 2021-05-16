609465df639fb3b5a2e62ba638e201737cd99356
ssujinn <efsonata38@gmail.com>
2021-05-16 12:51:46 +0900

    Add Interface between content and UI

--- SwiftUIBulleyeV1/ContentView.swift
+++ SwiftUIBulleyeV1/ContentView.swift
@@ -14,12 +14,17 @@ struct ContentView: View {
     // stage for User Interface views
     @State var alertIsVisible: Bool = false
     @State var sliderValue: Double = 50.0
+    @State var target: Int = Int.random(in: 1...100)
+    var sliderValueRounded: Int {
+        Int(self.sliderValue.rounded())
+    }
     var body: some View {
         VStack {
             Spacer()
             HStack {
                 Text("Put the bull eye as close as you can do!")
-                Text("100")
+                //Text("100")
+                Text("\(self.target)")
 
             }
             Spacer()
@@ -32,7 +37,8 @@ struct ContentView: View {
             Spacer()
             // Button row
             Button(action: {
-                print("Button pressed")
+                //print("Button pressed")
+                print(" Points awarded: \(self.pointsForCurrentRound())")
                 self.alertIsVisible = true
             }) {
                 Text("Hit me!")
@@ -40,12 +46,12 @@ struct ContentView: View {
             // State for alert
             .alert(isPresented: self.$alertIsVisible){
                 Alert(title: Text("Hello there!"),
-                      message: Text("The slider's value is \(Int(self.sliderValue.rounded()))."),
+                      message: Text(self.scoringMessage()),
                       dismissButton: .default(Text("Awsome!")))
             }   // End of .alert()
             Spacer()
             // Score row
-            // TODO: Add view for the score, rounds, and start over and info button
+            // TODO: Add view for the score, rounds, and start over and info buttons sliderValueRounded - self.target }
             HStack {
                 Button(action:{}){
                     Text("Start over")
@@ -65,6 +71,23 @@ struct ContentView: View {
     }   // End of body
     
     // Methods
+    func pointsForCurrentRound() -> Int {
+        
+        var difference: Int
+        
+        if self.sliderValueRounded > self.target {
+            difference = self.sliderValueRounded - self.target }
+        else if self.target > self.sliderValueRounded {
+            difference = self.target - self.sliderValueRounded }
+        else {
+            difference = 0
+        }
+        return 100 - difference
+    }
+    
+    func scoringMessage() -> String {
+        return "The slider's value is \(self.sliderValueRounded).\n" + "The target value is \(self.target).\n" + "You scored \(pointsForCurrentRound()) points this round."
+    }
 } // End of struct
 
 // Preview
